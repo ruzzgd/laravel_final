f5af02066542f620014d86fa390de729d8e0485a
Ruzz-GD <ruzhell0909@gmail.com>
2025-12-17 19:29:38 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'API route working!']);
+});
+
+// Auth
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route; 
-use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
 
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', function () {
+    return view('welcome');
+});
